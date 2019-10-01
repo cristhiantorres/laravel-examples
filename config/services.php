b44a97c9998faa8b512cca85fac1962389c16944
Cristhian Torres <cristhiantorres95@gmail.com>
2019-09-30 21:01:58 -0400

    Test

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => route('login.github_callback'),
+        'redirect' => 'https://laravel-examples.herokuapp.com/login/github/callback',
     ]
 
 ];
